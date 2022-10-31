d00e25e701ff50cfb4cd46257e5d0fd57f9fcf7e
VaniVenna <vennavani56@gmail.com>
2022-10-31 15:32:44 +0530

    folder structure

---
+++ components/common/Footer/index.js
@@ -0,0 +1,7 @@
+const Footer = () => {
+  return (
+    <footer>Copyright 2022 © CNC Machinist</footer>
+  )
+}
+
+export default Footer
--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+}
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
 
 module.exports = nextConfig
